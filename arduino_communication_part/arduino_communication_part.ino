1b9d9372a835103149e8f0655bbdc361ca9b8878
Shoaib <shoaib.mustafa7@hotmail.com>
2020-01-14 08:25:41 +0500

    fixed string receive problem wtih readstring()

--- arduino_communication_part/arduino_communication_part.ino
+++ arduino_communication_part/arduino_communication_part.ino
@@ -1,33 +1,36 @@
 /*this is the bluetooth communication part of the project 
 using two serial ports to accomodate the hc-05
 Progress report 
-1: 12/1/2020 -serial communication working okay unable to convert incoming string to human readable but is able to 
+1: 12/13/2020 -serial communication working okay unable to convert incoming string to human readable but is able to 
    compare and do an action based on that
+
+2: 12/14/2020 -fixed serial communication. readstring solved the problem and now receiving full string with this 
 */
-String c = "";
+
+String s = "";
 
 void setup ()
 {
   Serial.begin (9600);   //com port serial
   Serial1.begin(9600);   //bluetooth module serial 
-  pinMode(13, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.println("program start");
 }
 void loop ()
 {
 
-  if (Serial1.available()) {
-    c = Serial1.read();
-    delay(10);
+  s = "";
+  while(Serial1.available()) {
+    s = Serial1.readString(); 
   }
-  
-  Serial.print(c);
-  
+  if(s!= ""){
+    Serial.println(s);
+  }
 
-  if(c.length() > 0) {
-    if(c == "all on"){
-      digitalWrite(13, HIGH);
-    }
+  if(s == "*all on"){
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("working");
   }
+  
 
 }
